17888e3af86dc69aa8c100e28be3d850830c523f
Alexander Ng <alexng353@gmail.com>
2025-03-20 01:22:07 -0700

    Lectures 25-27

--- notes/gen.ts
+++ notes/gen.ts
@@ -4,21 +4,21 @@ const args = Bun.argv;
 const FORCE = args.includes("--force") || args.includes("-f");
 
 if (args.length < 2) {
-  console.error("Usage: bun gen <n>");
+  console.error("Usage: bun gen <n> | bun gen -n");
   process.exit(1);
 }
 
-const arg = args.at(-1);
+const arg1 = args.at(-1);
 
 let n: number;
-if (arg === "next" || arg === "-n") {
+if (arg1 === "next" || arg1 === "-n") {
   const dir = await fs.readdir(".");
   const files = dir.filter((b) => b.match(/^Lecture \d+\.tex$/));
 
   files.sort((a, b) => Number.parseInt(a.split(" ")[1]) - Number.parseInt(b.split(" ")[1]));
   n = Number.parseInt(files.at(-1)?.split(" ")[1]!) + 1;
 } else {
-  n = Number.parseInt(arg ?? "NaN");
+  n = Number.parseInt(arg1 ?? "NaN");
 }
 
 if (Number.isNaN(n)) {
@@ -48,8 +48,17 @@ const balls = `\\documentclass[12pt]{article}
 \\usepackage{wasysym}
 \\usepackage{ulem}
 \\usepackage{xspace}
-\\usepackage{csquotes}
 \\usepackage{booktabs}
+
+\\ifPDFTeX % ensure generation of machine readable output
+\\input{glyphtounicode}
+\\pdfgentounicode=1
+\\usepackage[T1]{fontenc}
+\\usepackage[utf8]{inputenc}
+\\usepackage{lmodern}
+\\fi
+
+\\usepackage{csquotes}
 
 \\DeclareMathOperator{\\dist}{dist}
 \\DeclareMathOperator{\\Nul}{Nul}
@@ -70,7 +79,6 @@ const balls = `\\documentclass[12pt]{article}
 \\newcommand{\\soln}{\\textit{Soln.}\\xspace}
 
 \\renewcommand{\\arraystretch}{1.25} % Adjust row spacing
-
 
 \\hypersetup{
     colorlinks=true,
