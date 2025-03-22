c304399fec6de2e7c4d7e716c7978e7fe7dce60e
Alexander Ng <alexng353@gmail.com>
2025-03-21 17:21:28 -0700

    latex watch script

---
+++ notes/latexw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if ! command -v entr &> /dev/null
+then
+    echo "Error: 'entr' is not installed."
+    echo "You can install it with:"
+    echo ""
+    echo "  # Debian/Ubuntu"
+    echo "  sudo apt install entr"
+    echo ""
+    echo "  # macOS (with Homebrew)"
+    echo "  brew install entr"
+    echo ""
+    echo "  # Arch Linux"
+    echo "  sudo pacman -S entr"
+    echo ""
+    exit 1
+fi
+
+if [[ -z "$1" ]]
+then
+  echo "Usage: watch_latex <filename>"
+  return 1
+fi
+
+ls -G "$1" | entr -r latexmk -quiet -pdf "$1"
+
